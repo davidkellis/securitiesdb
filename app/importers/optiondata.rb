d5537e15b03b4e54d403a3c59064ea48048935a4
David Ellis <davidkellis@gmail.com>
2016-03-28 23:42:12 -0500

    started a skeleton of an importer for optiondata.net options data

---
+++ app/importers/optiondata.rb
@@ -0,0 +1,9 @@
+# this imports historical options prices from the zip files downloaded from http://optiondata.net/
+class OptionDataImporter
+  def initialize(zip_file_paths)
+    @zip_file_paths = zip_file_paths
+  end
+
+  def import
+  end
+end
